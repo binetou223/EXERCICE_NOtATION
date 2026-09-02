3c1b700ce4b82d8e64f6b90272ea524ddc36933e
Binetou <binetoudiasn@gmail.com>
2026-09-02 22:57:11 +0000

    feat: transporter les données de soumission

---
+++ systeme-notation-universitaire/src/DTO/SoumettreCopieDTO.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\DTO;
+    readonly class SoumettreCopieDTO
+    {
+        private function __construct(
+            public float $noteBrute,
+            public \DateTimeImmutable $dateDepot,
+            public \DateTimeImmutable $dateLimite
+        ) {}
+
+
+            
+    }
